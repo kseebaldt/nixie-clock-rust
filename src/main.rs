88fbd5d678daf7006f496cf056532e869345e67b
Kurtis Seebaldt <kseebaldt@gmail.com>
2024-05-08 21:31:18 -0500

    Add wifi and ntp

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
-fn main() {
+use esp_idf_svc::sntp;
+use esp_idf_svc::sys::EspError;
+
+#[toml_cfg::toml_config]
+pub struct Config {
+    #[default("Wokwi-GUEST")]
+    wifi_ssid: &'static str,
+    #[default("")]
+    wifi_pass: &'static str,
+}
+
+use log::info;
+
+fn main() -> Result<(), EspError> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
@@ -7,4 +20,52 @@ fn main() {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
+
+    // Keep it around or else the wifi will stop
+    let _wifi = wifi_create()?;
+
+    // Keep it around or else the SNTP service will stop
+    let _sntp = sntp::EspSntp::new_default()?;
+    info!("SNTP initialized");
+
+    loop {
+        // To get a better formatting of the time, you can use the `chrono` or `time` Rust crates
+        info!("Current time: {:?}", std::time::SystemTime::now());
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
+}
+
+fn wifi_create() -> Result<esp_idf_svc::wifi::EspWifi<'static>, EspError> {
+    use esp_idf_svc::eventloop::*;
+    use esp_idf_svc::hal::prelude::Peripherals;
+    use esp_idf_svc::nvs::*;
+    use esp_idf_svc::wifi::*;
+
+    let sys_loop = EspSystemEventLoop::take()?;
+    let nvs = EspDefaultNvsPartition::take()?;
+
+    let peripherals = Peripherals::take()?;
+
+    let mut esp_wifi = EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs.clone()))?;
+    let mut wifi = BlockingWifi::wrap(&mut esp_wifi, sys_loop.clone())?;
+
+    let app_config = CONFIG;
+
+    wifi.set_configuration(&Configuration::Client(ClientConfiguration {
+        ssid: app_config.wifi_ssid.try_into().unwrap(),
+        password: app_config.wifi_pass.try_into().unwrap(),
+        auth_method: AuthMethod::None,
+        ..Default::default()
+    }))?;
+
+    wifi.start()?;
+    info!("Wifi started");
+
+    wifi.connect()?;
+    info!("Wifi connected");
+
+    wifi.wait_netif_up()?;
+    info!("Wifi netif up");
+
+    Ok(esp_wifi)
 }
