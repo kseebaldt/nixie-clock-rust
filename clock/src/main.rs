dd947d181ada856a2db37ff09d5a43b4d0369abd
Kurtis Seebaldt <kurtis@seebaldt.com>
2024-08-20 17:20:33 -0500

    Add config endpoint to http server

--- clock/src/main.rs
+++ clock/src/main.rs
@@ -1,22 +1,47 @@
 use esp_idf_svc::sntp;
+use std::sync::{Arc, Mutex};
 
 use anyhow::Result;
 
 use chrono::Utc;
 use chrono_tz::Tz;
 
-use drivers::{nixie_display::NixieDisplay, shift_register::ShiftRegister};
+use drivers::{nixie_display::NixieDisplay, shift_register::ShiftRegister, storage::{InMemoryStorage, Storage}};
 use embedded_svc::http::Method;
 use esp_idf_svc::hal::{gpio::*, modem::Modem, prelude::*};
 use esp_idf_svc::http::server::EspHttpServer;
 use esp_idf_svc::io::Write;
+use esp_idf_svc::nvs::EspCustomNvsPartition;
+use nixie_clock_rust::storage::NvsStorage;
+use serde::{Deserialize, Serialize};
+use postcard::{from_bytes, to_vec};
 
 #[toml_cfg::toml_config]
-pub struct Config {
+pub struct WifiConfig {
     #[default("Wokwi-GUEST")]
     wifi_ssid: &'static str,
     #[default("")]
     wifi_pass: &'static str,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct Config<'a> {
+    wifi_ssid: &'a str,
+    wifi_pass: &'a str,
+    tz: &'a str,
+    led_color: u32,
+}
+
+impl Default for Config<'_> {
+    fn default() -> Self {
+        let wifi_config = WIFI_CONFIG;
+        Config {
+            wifi_ssid: &wifi_config.wifi_ssid,
+            wifi_pass: &wifi_config.wifi_pass,
+            tz: "America/Chicago",
+            led_color: 0x00000088,
+        }
+    }
 }
 
 use log::info;
@@ -39,6 +64,11 @@ fn main() -> Result<()> {
     let pins = peripherals.pins;
     let modem = peripherals.modem;
     let ledc = peripherals.ledc;
+
+    let storage: Arc<Mutex<dyn Storage + Send>> = match EspCustomNvsPartition::take("config") {
+        Ok(partition) => Arc::new(Mutex::new(NvsStorage::new(partition, "storage")?)),
+        Err(_) => Arc::new(Mutex::new(InMemoryStorage::new())),
+    };
 
     // Create the shift register
     let mut data_pin = PinDriver::output(pins.gpio16)?;
@@ -66,7 +96,8 @@ fn main() -> Result<()> {
     rgb.set_color(0x00000088)?;
 
     // Keep it around or else the wifi will stop
-    let _wifi = wifi_create(modem)?;
+    let app_config = Config::default();
+    let _wifi = wifi_create(modem, &app_config)?;
 
     // Keep it around or else the SNTP service will stop
     let _sntp = sntp::EspSntp::new_default()?;
@@ -88,6 +119,22 @@ fn main() -> Result<()> {
             .map(|_| ())
     })?;
 
+    let storage2 = storage.clone();
+    server.fn_handler("/config", Method::Get, move |req
+    | {
+        let mut buf:[u8; 256] = [0; 256];
+        let s = storage2.lock().unwrap();
+        let config = match s.get_raw("config", &mut buf) {
+            Ok(Some(v)) => from_bytes::<Config>(v).unwrap(),
+            _ => Config::default(),
+        };
+        let j = serde_json::to_string(&config).unwrap();
+
+        req.into_ok_response()?
+            .write_all(j.as_bytes())
+            .map(|_| ())
+    })?;
+
     loop {
         // To get a better formatting of the time, you can use the `chrono` or `time` Rust crates
         let local_time = Utc::now().with_timezone(&tz);
@@ -99,7 +146,7 @@ fn main() -> Result<()> {
     }
 }
 
-fn wifi_create(modem: Modem) -> Result<esp_idf_svc::wifi::EspWifi<'static>> {
+fn wifi_create(modem: Modem, app_config: &Config) -> Result<esp_idf_svc::wifi::EspWifi<'static>> {
     use esp_idf_svc::eventloop::*;
     use esp_idf_svc::nvs::*;
     use esp_idf_svc::wifi::*;
@@ -109,8 +156,6 @@ fn wifi_create(modem: Modem) -> Result<esp_idf_svc::wifi::EspWifi<'static>> {
 
     let mut esp_wifi = EspWifi::new(modem, sys_loop.clone(), Some(nvs.clone()))?;
     let mut wifi = BlockingWifi::wrap(&mut esp_wifi, sys_loop.clone())?;
-
-    let app_config = CONFIG;
 
     wifi.set_configuration(&Configuration::Client(ClientConfiguration {
         ssid: app_config.wifi_ssid.try_into().unwrap(),
