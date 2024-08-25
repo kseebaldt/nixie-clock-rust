e15ec1bcef91cf6bee30efb29bf492b505f2815c
Kurtis Seebaldt <kurtis@seebaldt.com>
2024-08-25 08:45:01 -0500

    Add configuration form to webapp

--- webapp/src/App.svelte
+++ webapp/src/App.svelte
@@ -1,54 +1,75 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg';
-  import viteLogo from './assets/vite.svg';
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "./assets/vite.svg";
   import Form from "./lib/Form.svelte";
 
-  import { onMount } from 'svelte';
+  import { onMount } from "svelte";
 
   function handleOnSubmit() {
-		console.log("I'm the handleOnSubmit() in App.svelte");
-	}
+    console.log("I'm the handleOnSubmit() in App.svelte");
+  }
 
-	let weather = {
-    hourly: {
-      temperature_2m: []
-    }
+  let config = {
+    wifi_ssid: "",
+    wifi_pass: "",
+    tz: "",
+    led_color: 0,
   };
 
-	onMount(async () => {
-		const res = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m`);
-		weather = await res.json();
-	});  
+  onMount(async () => {
+    const res = await fetch("/config");
+    config = await res.json();
+  });
+
+  const saveConfig = async (event: any) => {
+    event.preventDefault();
+    console.log(event);
+    console.log(config);
+    // const res = await fetch("/config", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(config),
+    // });
+    // console.log(await res.json());
+  };
 </script>
 
 <main>
   <h1>Nixie Clock</h1>
 
-  <Form on:submit={handleOnSubmit}>
-  </Form>
+  <form on:submit={saveConfig}>
+    <label for="name">SSID</label>
+    <input id="wifi_ssid" name="wifi_ssid" bind:value={config.wifi_ssid} />
 
-  <div class="photos">
-    {#each weather.hourly.temperature_2m as temp}
-      <div>
-        <p>{temp}</p>
-      </div>
-    {:else}
-      <!-- this block renders when photos.length === 0 -->
-      <p>loading...</p>
-    {/each}
-  </div>
+    <label for="name">Password</label>
+    <input id="wifi_pass" name="wifi_pass" bind:value={config.wifi_pass} />
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+    <label for="tz">Time Zone</label>
+    <select id="tz" name="tz" bind:value={config.tz}>
+      <option></option>
+      <option>US/Alaska</option>
+      <option>US/Arizona</option>
+      <option>US/Central</option>
+      <option>US/East-Indiana</option>
+      <option>US/Eastern</option>
+      <option>US/Hawaii</option>
+      <option>US/Indiana-Starke</option>
+      <option>US/Michigan</option>
+      <option>US/Mountain</option>
+      <option>US/Pacific</option>
+      <option>US/Pacific-New</option>
+    </select>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <label for="name">LED Color</label>
+    <input
+      id="led_color"
+      name="led_color"
+      type="color"
+      bind:value={config.led_color}
+    />
+
+    <button type="submit">Save</button>
+  </form>
 </main>
-
-<style>
-  .read-the-docs {
-    color: #888;
-  }
-</style>
