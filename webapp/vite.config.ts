afd996dba51fc49dde513d5772ec7923d82b4ecb
Kurtis Seebaldt <kurtis@seebaldt.com>
2024-08-24 20:42:08 -0500

    Add vite dev server proxy

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte(), viteSingleFile()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const nixieHost = `http://${env.VITE_NIXIE_IP}`;
+  console.log("proxy to: ", nixieHost);
+  return {
+    plugins: [svelte(), viteSingleFile()],
+    server: {
+      proxy: {
+        '/config': {
+          target: nixieHost,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
+});
